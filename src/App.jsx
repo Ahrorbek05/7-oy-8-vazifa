28eb6d83c1aff0a38aa4edbac61516de1e2b1e45
Ahrorbek05 <akramjonovahrorbek96@gmail.com>
2024-08-27 22:26:05 +0500

    homework done!

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Input from './component/Input'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Input></Input>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import currency from '../country-currency.json'
+
+function Input() {
+
+  return (
+    <div>
+        <input type="text" placeholder="Type to search..." className="input input-bordered w-full max-w-xs"/>
+    </div>
+  )
+}
+
+export default Input
